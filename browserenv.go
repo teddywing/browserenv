18033da5978da3d7c090d0e6e3d516d1b3c7ee19
Teddy Wing <git@teddywing.com>
2020-12-20 19:53:36 +0100

    Add license (MPL 2.0)

--- browserenv.go
+++ browserenv.go
@@ -1,3 +1,9 @@
+// Copyright (c) 2020  Teddy Wing
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 // Package browserenv allows URLs and files to be opened in a local web
 // browser. The system's default browser is used. If the BROWSER environment
 // variable is set, the command it specifies is used instead.
